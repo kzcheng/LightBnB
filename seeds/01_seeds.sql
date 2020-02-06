872dc59311908845c8bd3388aab901380f4e1c07
kzcheng <kevinzifancheng@gmail.com>
2020-02-05 19:28:32 -0800

    Created seeded data

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email, password) 
+    VALUES ('Kevin Cheng', 'test@test.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) 
+    VALUES ('Akira Chisaka', 'akira@chisaka.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) 
+    VALUES ('Int Main', 'int@main.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
