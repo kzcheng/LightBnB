ea49d20e01f38ec79e39f6447c5e73f6708d7dc2
kzcheng <kevinzifancheng@gmail.com>
2020-02-07 09:47:07 -0800

    Make getUserWithId use SQL Database. It's not tested yet because I can't find where to test it.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -53,7 +53,19 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+    SELECT
+      *
+    FROM
+      users
+    WHERE
+      users.id = $1
+  `, [id])
+    .then(data => {
+      const user = data.rows;
+      console.log(user);
+      return user[0];
+    });
 };
 exports.getUserWithId = getUserWithId;
 
